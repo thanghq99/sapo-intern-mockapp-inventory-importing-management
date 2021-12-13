60fa0cd575378798a2a8f54b7a4c4d699b90f17d
hieutran29 <hieu.xteen28@gmail.com>
2021-12-13 08:50:34 +0700

    Initialized backend

---
+++ backend/storemanagement/src/test/java/com/sapo/storemanagement/StoremanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sapo.storemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoremanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
