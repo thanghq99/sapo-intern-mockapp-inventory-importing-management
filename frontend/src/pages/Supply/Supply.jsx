724be3a2e1eac89b38ac55a4267fc49834f00595
babybull <abc@mm.com>
2021-12-17 17:00:19 +0700

    upload 17-12-21_2

--- frontend/src/pages/Supply/Supply.jsx
+++ frontend/src/pages/Supply/Supply.jsx
@@ -1,7 +1,18 @@
-import { Button } from '@mui/material'
+import { Autocomplete, Button, TextField } from '@mui/material'
 import React from 'react'
+import TableSupply from '../../components/table/TableSupply'
+import Table from '../../components/table/TableSupply'
 import "./supply.scss"
 
+const top100Films = [
+    { title: 'The Shawshank Redemption', year: 1994 },
+    { title: 'The Godfather', year: 1972 },
+    { title: 'The Godfather: Part II', year: 1974 },
+    { title: 'The Dark Knight', year: 2008 },
+    { title: '12 Angry Men', year: 1957 },
+    { title: "Schindler's List", year: 1993 },
+    { title: 'Pulp Fiction', year: 1994 },
+]
 export default function Supply() {
     return (
         <div className='supply_page'>
@@ -26,9 +37,35 @@ export default function Supply() {
                         </div>
                     </div>
                     <div className="right_activity">
-                        <Button variant="contained"><i className="far fa-plus-square"></i>Contained</Button>
+                        <Button variant="contained"><i className="fas fa-plus-circle"></i> <span>Them nha cung cap</span> </Button>
                     </div>
                 </div>
+                <hr />
+                <div className="find_section">
+                    <div className="search_section">
+                        <i className="fas fa-search"></i>
+                        <input className='search_input' type="text" placeholder='search here ...' />
+                    </div>
+                    <div className="filter_section">
+                        <i className="fas fa-filter"></i>
+                        <Autocomplete
+                            className="filter_input"
+                            multiple
+                            id="tags-standard"
+                            options={top100Films}
+                            getOptionLabel={(option) => option.title}
+                            renderInput={(params) => (
+                                <TextField
+                                    {...params}
+                                    variant="standard"
+                                    label="Loc nhieu gia tri"
+                                    placeholder="Favorites"
+                                />
+                            )}
+                        />
+                    </div>
+                </div>
+                <TableSupply className="table" />
             </div>
         </div>
     )
