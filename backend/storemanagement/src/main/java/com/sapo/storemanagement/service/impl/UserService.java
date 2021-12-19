ae623c2b6370cde3a22d8437018e999592f76a76
hieutran29 <hieu.xteen28@gmail.com>
2021-12-19 22:19:20 +0700

    Implemented UserService

--- backend/storemanagement/src/main/java/com/sapo/storemanagement/service/impl/UserService.java
+++ backend/storemanagement/src/main/java/com/sapo/storemanagement/service/impl/UserService.java
@@ -1,10 +1,23 @@
 package com.sapo.storemanagement.service.impl;
 
+import com.sapo.storemanagement.entities.AppUserDetails;
+import com.sapo.storemanagement.entities.User;
+import com.sapo.storemanagement.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     /**
      * Locates the user based on the username. In the actual implementation, the search
      * may be case-sensitive, or case-insensitive depending on how the
@@ -19,6 +32,8 @@ public class UserService implements UserDetailsService {
      */
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        User user = userRepository.findByUsername(username)
+            .orElseThrow(() -> new UsernameNotFoundException("Username" + username + " not found"));
+        return new AppUserDetails(user);
     }
 }
