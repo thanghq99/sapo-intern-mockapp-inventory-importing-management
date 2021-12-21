13e3df729e38b52dac9722b420f1b46c803387b4
hieutran29 <hieu.xteen28@gmail.com>
2021-12-21 09:53:44 +0700

    Finished API for Variant

--- backend/storemanagement/src/main/java/com/sapo/storemanagement/controller/VariantController.java
+++ backend/storemanagement/src/main/java/com/sapo/storemanagement/controller/VariantController.java
@@ -1,9 +1,11 @@
 package com.sapo.storemanagement.controller;
 
+import com.sapo.storemanagement.entities.Variant;
 import com.sapo.storemanagement.service.VariantService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/variants")
@@ -13,5 +15,30 @@ public class VariantController {
     @Autowired
     public VariantController(VariantService variantService) {
         this.variantService = variantService;
+    }
+
+    @GetMapping("/{id}")
+    public Variant findVariantById(@PathVariable long id){
+        return variantService.getVariantById(id);
+    }
+
+    @GetMapping
+    public List<Variant> findAllVariants(){
+        return variantService.listAllVariants();
+    }
+
+    @PostMapping
+    public Variant createVariant(@RequestBody Variant variant){
+        return variantService.saveVariant(variant);
+    }
+
+    @PutMapping("/{id}")
+    public Variant updateVariant(@PathVariable long id, @RequestBody Variant variant){
+        return variantService.updateVariant(id, variant);
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteVariant(@PathVariable long id){
+        return variantService.deleteVariant(id);
     }
 }
