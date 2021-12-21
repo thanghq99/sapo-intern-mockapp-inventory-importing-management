cf7c9633b7f4f658646534155255fad8c5edd837
hieutran29 <hieu.xteen28@gmail.com>
2021-12-21 09:03:08 +0700

    Added API for Role

--- backend/storemanagement/src/main/java/com/sapo/storemanagement/service/impl/RoleServiceImpl.java
+++ backend/storemanagement/src/main/java/com/sapo/storemanagement/service/impl/RoleServiceImpl.java
@@ -1,8 +1,37 @@
 package com.sapo.storemanagement.service.impl;
 
+import com.sapo.storemanagement.entities.Role;
+import com.sapo.storemanagement.exception.RecordNotFoundException;
+import com.sapo.storemanagement.repository.RoleRepository;
 import com.sapo.storemanagement.service.RoleService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RoleServiceImpl implements RoleService {
+    private final RoleRepository roleRepository;
+
+    @Autowired
+    public RoleServiceImpl(RoleRepository roleRepository) {
+        this.roleRepository = roleRepository;
+    }
+
+    @Override
+    public Role getRoleById(Long id) {
+        return roleRepository
+            .findById(id)
+            .orElseThrow(() -> new RecordNotFoundException("Role not found"));
+    }
+
+    @Override
+    public Role getRoleByName(String name) {
+        return roleRepository
+            .findByName(name)
+            .orElseThrow(() -> new RecordNotFoundException("Supplier not found"));
+    }
+
+    @Override
+    public Role saveRole(Role role) {
+        return roleRepository.save(role);
+    }
 }
