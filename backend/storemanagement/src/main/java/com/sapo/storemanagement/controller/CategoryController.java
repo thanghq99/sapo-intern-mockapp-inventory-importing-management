b785a17f0ff7f12d864c213d763c6ad90784f109
hieutran29 <hieu.xteen28@gmail.com>
2021-12-21 09:13:43 +0700

    Added API for Category

--- backend/storemanagement/src/main/java/com/sapo/storemanagement/controller/CategoryController.java
+++ backend/storemanagement/src/main/java/com/sapo/storemanagement/controller/CategoryController.java
@@ -1,9 +1,11 @@
 package com.sapo.storemanagement.controller;
 
+import com.sapo.storemanagement.entities.Category;
 import com.sapo.storemanagement.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/categories")
@@ -13,5 +15,20 @@ public class CategoryController {
     @Autowired
     public CategoryController(CategoryService categoryService) {
         this.categoryService = categoryService;
+    }
+
+    @GetMapping("/{id}")
+    public Category findSupplierById(@PathVariable long id){
+        return categoryService.getCategoryById(id);
+    }
+
+    @GetMapping
+    public List<Category> findAllSuppliers(){
+        return categoryService.listAllCategories();
+    }
+
+    @PostMapping
+    public Category createSupplier(@RequestBody Category category){
+        return categoryService.saveCategory(category);
     }
 }
--- backend/storemanagement/src/main/java/com/sapo/storemanagement/service/impl/CategoryServiceImpl.java
+++ backend/storemanagement/src/main/java/com/sapo/storemanagement/service/impl/CategoryServiceImpl.java
@@ -1,8 +1,37 @@
 package com.sapo.storemanagement.service.impl;
 
+import com.sapo.storemanagement.entities.Category;
+import com.sapo.storemanagement.exception.RecordNotFoundException;
+import com.sapo.storemanagement.repository.CategoryRepository;
 import com.sapo.storemanagement.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CategoryServiceImpl implements CategoryService {
+    private final CategoryRepository categoryRepository;
+
+    @Autowired
+    public CategoryServiceImpl(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    @Override
+    public List<Category> listAllCategories() {
+        return categoryRepository.findAll();
+    }
+
+    @Override
+    public Category getCategoryById(Long id) {
+        return categoryRepository
+            .findById(id)
+            .orElseThrow(() -> new RecordNotFoundException("Category not found"));
+    }
+
+    @Override
+    public Category saveCategory(Category category) {
+        return categoryRepository.save(category);
+    }
 }
