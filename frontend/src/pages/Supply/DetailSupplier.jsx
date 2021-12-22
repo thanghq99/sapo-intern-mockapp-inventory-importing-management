88e5b6cb31d6e4a63adb4d230c60b28ccd930df5
babybull <abc@mm.com>
2021-12-22 23:52:12 +0700

    update CreateSupplier n DetailSupplier

--- frontend/src/pages/Supply/DetailSupplier.jsx
+++ frontend/src/pages/Supply/DetailSupplier.jsx
@@ -6,6 +6,7 @@ import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
+import TableHistoryOrder from '../../components/table/TableHistoryOrder';
 
 
 function TabPanel(props) {
@@ -60,17 +61,17 @@ export default function DetailSupplier() {
                 </div>
                 <hr />
                 <div className="tagname_supplier">
-                    <h2>Ten nha cung cap</h2>
+                    <h2>Tên nhà cung cấp</h2>
                 </div>
                 <div className="info_supplier">
                     <div className="first_info">
                         <div className="first_info_upper">
                             <Grid className="upper_items" container spacing={2}>
                                 <Grid className="upper_item" item xs={4}>
-                                    Thong tin nha cung cap
+                                    Thông tin nhà cung cấp
                                 </Grid>
                                 <Grid className="upper_item" item xs={4}>
-                                    <p style={{ color: "#1ec709" }}>Dang giao dich</p>
+                                    <p style={{ color: "#1ec709" }}>Đang giao dịch</p>
                                 </Grid>
                                 <Grid className="upper_item" item xs={4}>
                                     <a href="#"><i className="fas fa-edit"></i></a>
@@ -84,11 +85,11 @@ export default function DetailSupplier() {
                             <Grid container spacing={2}>
                                 <Grid item xs={6}>
                                     <ul>
-                                        <li><strong>Ten nha cung cap: </strong>acs
+                                        <li><strong>Tên nhà cung cấp: </strong>Nhà cung cấp số 1
                                         </li>
-                                        <li><strong>Ma nha cung cap: </strong>acs
+                                        <li><strong>Mã nhà cung cấp: </strong>NCC1
                                         </li>
-                                        <li> <strong>So dien thoai: </strong>acs
+                                        <li> <strong>Số điện thoại: </strong>0987654321
                                         </li>
                                     </ul>
                                 </Grid>
@@ -96,9 +97,9 @@ export default function DetailSupplier() {
                                     <ul>
                                         <li><strong>Email: </strong>acs
                                         </li>
-                                        <li><strong>So Fax: </strong>acs
+                                        <li><strong>Số Fax: </strong>11.22.33.45.5.44
                                         </li>
-                                        <li><strong>Dia chi: </strong>acs
+                                        <li><strong>Địa chỉ: </strong>số 1 Đại Cồ Việt, Hai Bà Trưng, Hà Nội
                                         </li>
                                     </ul>
                                 </Grid>
@@ -106,11 +107,11 @@ export default function DetailSupplier() {
                         </div>
                     </div>
                     <div className="first_info second_info">
-                        <div style={{ paddingBottom: "2em" }}>
-                            <strong>Thong tin bo sung</strong>
+                        <div style={{ paddingBottom: "1em" }}>
+                            <strong>Thông tin bổ sung</strong>
                         </div>
                         <Box
-                            sx={{ flexGrow: 1, bgcolor: 'background.paper', display: 'flex', height: 224 }}
+                            sx={{ flexGrow: 1, bgcolor: 'background.paper', display: 'flex', height: "100%" }}
                         >
                             <Tabs
                                 orientation="vertical"
@@ -120,13 +121,13 @@ export default function DetailSupplier() {
                                 aria-label="Vertical tabs example"
                                 sx={{ borderRight: 1, borderColor: 'divider' }}
                             >
-                                <Tab label="Lich su nhap hang" {...a11yProps(0)} />
-                                <Tab label="Cong no" {...a11yProps(1)} />
-                                <Tab label="Lien he" {...a11yProps(2)} />
-                                <Tab label="Ghi chu" {...a11yProps(3)} />
+                                <Tab label="Lịch sử nhập hàng" {...a11yProps(0)} />
+                                <Tab label="Công nợ" {...a11yProps(1)} />
+                                <Tab label="Liên hệ" {...a11yProps(2)} />
+                                <Tab label="Ghi chú" {...a11yProps(3)} />
                             </Tabs>
-                            <TabPanel value={value} index={0}>
-                                Item One
+                            <TabPanel style={{ width: "100%" }} value={value} index={0}>
+                                <TableHistoryOrder />
                             </TabPanel>
                             <TabPanel value={value} index={1}>
                                 Item Two
