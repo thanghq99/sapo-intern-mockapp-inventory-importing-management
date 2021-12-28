5de4dcd042f393762b61fe13aee5176a67e01856
babybull <abc@mm.com>
2021-12-28 11:12:32 +0700

    update 2

--- backend/storemanagement/src/main/java/com/sapo/storemanagement/entities/Order.java
+++ backend/storemanagement/src/main/java/com/sapo/storemanagement/entities/Order.java
@@ -1,23 +1,28 @@
 package com.sapo.storemanagement.entities;
 
+import com.sapo.storemanagement.exception.BadNumberException;
 
 import javax.persistence.*;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Entity
-
 @Table(name = "orders", indexes = {
-    @Index(name = "orders_code_unique", columnList = "code", unique = true)
+        @Index(name = "orders_code_unique", columnList = "code", unique = true)
 })
 public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-
     @Column(name = "id", nullable = false)
     private Long id;
 
     @Column(name = "code", nullable = false, unique = true, length = 8)
+    @NotBlank(message = "Order code cannot be blank")
+    @Size(max = 8, message = "Order code length cannot exceed {max}")
     private String code;
 
     @ManyToOne(optional = false)
@@ -25,9 +30,13 @@ public class Order {
     private Supplier supplier;
 
     @Column(name = "total_amount", nullable = false)
-    private Double totalAmount;
+    @NotNull(message = "Total amount cannot be null")
+    @Min(value = 0, message = "Total amount cannot be less than {value}")
+    private Double totalAmount = 0.0;
 
     @Column(name = "paid_amount")
+    @NotNull(message = "Paid amount cannot be null")
+    @Min(value = 0, message = "Total amount cannot be less than {value}")
     private Double paidAmount = 0.0;
 
     @Column(name = "expected_time", nullable = false)
@@ -55,20 +64,12 @@ public class Order {
     public Order() {
     }
 
-    public Order(String code, Supplier supplier, Double totalAmount,
+    public Order(String code, Supplier supplier,
                  LocalDate expectedTime, User createdBy) {
         this.code = code;
         this.supplier = supplier;
-        this.totalAmount = totalAmount;
         this.expectedTime = expectedTime;
         this.createdBy = createdBy;
-    }
-    public Order( Double totalAmount, Double paidAmount, LocalDate expectedTime) {
-
-        this.totalAmount = totalAmount;
-        this.paidAmount = paidAmount;
-        this.expectedTime = expectedTime;
-
     }
 
     public Long getId() {
@@ -87,30 +88,21 @@ public class Order {
         return totalAmount;
     }
 
-    public void setTotalAmount(Double totalAmount) {
+    public void setTotalAmount(double totalAmount) {
         this.totalAmount = totalAmount;
     }
-
 
     public Double getPaidAmount() {
         return paidAmount;
     }
 
-    public void setPaidAmount(Double paidAmount) {
+    public void setPaidAmount(double paidAmount) {
         this.paidAmount = paidAmount;
-    }
-
-    public void increasePaidAmount(double offset) {
-        // check if offset was >= 0
-
-        this.setTotalAmount(this.paidAmount + offset);
-    }
-
-    public void decreasePaidAmount(double offset) {
-        // check if offset was >= 0
-
-        if(this.totalAmount >= offset) {
-            this.setTotalAmount(this.paidAmount - offset);
+        if(this.paidAmount < this.totalAmount) {
+            this.setTransactionStatus(TransactionStatus.PARTIAL_PAID);
+        }
+        else {
+            this.setTransactionStatus(TransactionStatus.PAID);
         }
     }
 
