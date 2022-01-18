fdcc3ae6014dec6e9e7f2016e3b0d2fde7d4369f
babybull <abc@mm.com>
2022-01-18 22:12:21 +0700

    fix fe

--- backend/storemanagement/src/main/java/com/sapo/storemanagement/repository/UserRepository.java
+++ backend/storemanagement/src/main/java/com/sapo/storemanagement/repository/UserRepository.java
@@ -1,7 +1,10 @@
 package com.sapo.storemanagement.repository;
 
+import com.sapo.storemanagement.entities.Supplier;
 import com.sapo.storemanagement.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -15,4 +18,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByUsername(String username);
 
     boolean existsByEmail(String email);
+
+    @Query(value = "select * from users where record_status like :record_status ", nativeQuery = true)
+    Iterable<User> findByRecordStatus(@Param("record_status") String record_status);
 }
--- frontend/src/pages/User/User.jsx
+++ frontend/src/pages/User/User.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Grid, Box, Radio, Snackbar, Alert, RadioGroup, Paper, FormControlLabel, TextField, Avatar, Button, Modal, Input, TableContainer, Table, TableCell, TablePagination, TableRow, TableHead, TableBody } from '@mui/material';
+import { Grid, Box, Radio, Snackbar, Alert, IconButton, RadioGroup, Paper, FormControlLabel, TextField, Avatar, Button, Modal, Input, TableContainer, Table, TableCell, TablePagination, TableRow, TableHead, TableBody } from '@mui/material';
 import { styled } from '@mui/material/styles';
 import PersonIcon from '@mui/icons-material/Person';
 import DeleteIcon from '@mui/icons-material/Delete';
@@ -7,8 +7,10 @@ import InfoIcon from '@mui/icons-material/Info';
 import MailIcon from '@mui/icons-material/Mail';
 import LeaderboardIcon from '@mui/icons-material/Leaderboard';
 import BrowserUpdatedIcon from '@mui/icons-material/BrowserUpdated';
+import EditIcon from '@mui/icons-material/Edit';
 import { AddCircle } from '@mui/icons-material';
 import UsersApi from '../../api/UsersApi';
+import { Link, useHistory } from "react-router-dom";
 
 
 const Item = styled(Paper)(({ theme }) => ({
@@ -23,12 +25,14 @@ const columns = [
     { id: 'Id nhân viên', label: 'Id nhân viên' },
     { id: 'Tên nhân viên', label: 'Tên nhân viên' },
     { id: 'role', label: 'Vai trò nhân viên' },
-    { id: 'email', label: 'Email nhân viên' }
+    { id: 'email', label: 'Email nhân viên' },
+    { id: 'activity ', label: 'Thao tac' },
 ];
 
 const checkRole = (JSON.parse(sessionStorage.getItem("token"))?.role[0].name == "ADMIN");
 
 export default function User() {
+    const history = useHistory();
 
     const [page, setPage] = React.useState(0);
     const [rowsPerPage, setRowsPerPage] = React.useState(10);
@@ -53,7 +57,7 @@ export default function User() {
     const handleCloseModal = () => setOpenModal(false);
 
     // get all users********************************//
-    const [trigger, setTrigger] = React.useState(false); // trigger to re-render supplier's info
+    const [trigger, setTrigger] = React.useState(false); // trigger to re-render users
     const [listUsers, setListUsers] = React.useState([]);
     React.useEffect(() => {
         const fetchListUsers = async () => {
@@ -64,14 +68,18 @@ export default function User() {
     }, [trigger])
 
     // get an user********************************//
+    const [trigger2, setTrigger2] = React.useState(false);
     const [currentUser, setCurrentUser] = React.useState({});
     React.useEffect(() => {
         const fetchCurrentUser = async () => {
-            const res = await UsersApi.getUserById(JSON.parse(sessionStorage.getItem("token")).id);
+            const res = await UsersApi.getUserById(window.location.search.replace("?id=", "") ? window.location.search.replace("?id=", "") : JSON.parse(sessionStorage.getItem("token"))?.id);
             setCurrentUser(res.data);
+            setEditUserRole(res.data.role);
+            setEditUsername(res.data.name);
+            setEditUserEmail(res.data.email);
         }
         fetchCurrentUser();
-    }, [])
+    }, [trigger2, trigger])
 
     const [stateAlert, setStateAlert] = React.useState({ severity: "", variant: "", open: false, content: "" });
     // create an user******************************************//
@@ -108,10 +116,35 @@ export default function User() {
     }
 
     //delete user 
-    const deleteAnUser = async (e) => {
-        e.preventDefault();
-        await UsersApi.deleteUserStaff(currentUser.id)
-        setStateAlert({ severity: "error", variant: "filled", open: true, content: "Người dùng hoặc email này đã tồn tại" })
+    const deleteAnUser = async (id) => {
+        await UsersApi.deleteUserStaff(id);
+        setTrigger(!trigger);
+        setStateAlert({ severity: "success", variant: "filled", open: true, content: "Đã xóa người dùng này" })
+        setOpenModal(false);
+    }
+
+    // edit an user 
+
+    const [editUserRole, setEditUserRole] = React.useState([])
+    const [editUsername, setEditUsername] = React.useState(currentUser.username)
+    const [editUserEmail, setEditUserEmail] = React.useState(currentUser.email)
+    const [editUserPassword, setEditUserPassword] = React.useState()
+    const editAnUser = async () => {
+        const user = {
+            username: editUsername,
+            email: editUserEmail,
+            password: editUserPassword,
+            role: editUserRole
+        }
+        console.log(user);
+        try {
+            await UsersApi.updateUserStaff(currentUser.id, user)
+            setTrigger(!trigger);
+            setStateAlert({ severity: "success", variant: "filled", open: true, content: "Đã chinh sua người dùng này" })
+            setOpenModal(false);
+        } catch (error) {
+            setStateAlert({ severity: "error", variant: "filled", open: true, content: error.response.data })
+        }
     }
 
 
@@ -126,8 +159,8 @@ export default function User() {
                         <Item>
                             <Avatar
                                 alt="Remy Sharp"
-                                src="/static/images/avatar/1.jpg"
-                                sx={{ width: "14em", height: "14em", margin: "auto" }}
+                                src="https://datdainhacua.vn/images/icons/noUser.png?width=160"
+                                sx={{ width: "14em", height: "14em", margin: "auto", opacity: "0.5" }}
                             />
                             <Button sx={{ fontSize: "0.8em", textTransform: 'capitalize', margin: "0.5em 0 2em 0" }} variant="outlined">Thay ảnh</Button>
                             <h3><strong style={{ fontSize: "1.5em" }}>{currentUser.roles?.map((role) => (role.name))}</strong></h3>
@@ -168,59 +201,82 @@ export default function User() {
                                 </Grid>
                             </Grid>
                             {checkRole && <Grid sx={{ textAlign: "end", paddingTop: "2.4em" }}>
-                                <Button onClick={() => handleOpenModal("update")} variant="outlined" startIcon={<BrowserUpdatedIcon />} sx={{ fontSize: "0.8em", textTransform: 'capitalize', margin: "0.5em 0 2em 0" }}>Chỉnh sửa thông tin</Button>
-                                <Button onClick={() => setOpenModal(false)} variant="outlined" color="error" startIcon={<DeleteIcon />} sx={{ fontSize: "0.8em", textTransform: 'capitalize', margin: "0.5em 2em 2em 2em" }}>Xóa nhân viên</Button>
+                                <Button
+                                    onClick={() => {
+                                        handleOpenModal("update")
+                                    }
+                                    }
+                                    variant="outlined"
+                                    startIcon={<BrowserUpdatedIcon />}
+                                    sx={{ fontSize: "0.8em", textTransform: 'capitalize', margin: "0.5em 2em 2em 0" }}>
+                                    Chỉnh sửa thông tin
+                                </Button>
                             </Grid>}
                         </Item>
                     </Grid>
                 </Grid>
             </Box >
-            <Grid sx={{ padding: "4em 0em 1em 1em" }}>
-                <h3 style={{ marginBottom: "1em" }}><strong style={{ fontSize: "1.5em" }}>Thông tin nhân viên đang hoạt động</strong></h3>
-                <Paper sx={{ width: '100%', overflow: 'hidden' }}>
-                    <TableContainer sx={{ padding: "0 0 0 2em", maxHeight: 440 }}>
-                        <Table stickyHeader aria-label="sticky table">
-                            <TableHead>
-                                <TableRow>
-                                    {columns.map((column) => (
-                                        <TableCell
-                                            key={column.id}
-                                            align="left"
-                                        >
-                                            {column.label}
-                                        </TableCell>
-                                    ))}
-                                </TableRow>
-                            </TableHead>
-                            <TableBody>
-                                {listUsers
-                                    .reverse()
-                                    .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                                    .map((row) => {
-                                        return (
-                                            <TableRow>
-                                                <TableCell align="left">{row.id} </TableCell>
-                                                <TableCell align="left">{row.username} </TableCell>
-                                                <TableCell align="left">{row.roles?.map((r) => (r.name))} </TableCell>
-                                                <TableCell align="left">{row.email} </TableCell>
-                                            </TableRow>
-                                        );
-                                    })}
-                            </TableBody>
-                        </Table>
-                    </TableContainer>
-                    <TablePagination
-                        labelRowsPerPage="Số hàng một trang"
-                        rowsPerPageOptions={[10, 20, 50]}
-                        component="div"
-                        count={listUsers.length}
-                        rowsPerPage={rowsPerPage}
-                        page={page}
-                        onPageChange={handleChangePage}
-                        onRowsPerPageChange={handleChangeRowsPerPage}
-                    />
-                </Paper>
-            </Grid>
+            {checkRole &&
+                <Grid sx={{ padding: "4em 0em 1em 1em" }}>
+                    <h3 style={{ marginBottom: "1em" }}><strong style={{ fontSize: "1.5em" }}>Thông tin nhân viên đang hoạt động</strong></h3>
+                    <Paper sx={{ width: '100%', overflow: 'hidden' }}>
+                        <TableContainer sx={{ padding: "0 0 0 2em", maxHeight: 440 }}>
+                            <Table stickyHeader aria-label="sticky table">
+                                <TableHead>
+                                    <TableRow>
+                                        {columns.map((column) => (
+                                            <TableCell
+                                                key={column.id}
+                                                align="left"
+                                            >
+                                                {column.label}
+                                            </TableCell>
+                                        ))}
+                                    </TableRow>
+                                </TableHead>
+                                <TableBody>
+                                    {listUsers
+                                        .reverse()
+                                        .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                                        .map((row) => {
+                                            return (
+                                                <TableRow>
+                                                    <TableCell align="left">{row.id} </TableCell>
+                                                    <TableCell
+                                                        sx={{ cursor: "pointer" }}
+                                                        onClick={() => {
+                                                            history.push("/nguoi-dung?id=" + row.id);
+                                                            setTrigger2(!trigger2);
+                                                        }}
+                                                        align="left">
+                                                        {row.username}
+                                                    </TableCell>
+                                                    <TableCell align="left">{row.roles?.map((r) => (r.name))} </TableCell>
+                                                    <TableCell align="left">{row.email} </TableCell>
+                                                    <TableCell align="left">
+                                                        <IconButton onClick={() => deleteAnUser(row.id)} aria-label="delete">
+                                                            <DeleteIcon />
+                                                        </IconButton>
+                                                    </TableCell>
+                                                </TableRow>
+                                            );
+                                        })}
+                                </TableBody>
+                            </Table>
+                        </TableContainer>
+                        <TablePagination
+                            labelRowsPerPage="Số hàng một trang"
+                            rowsPerPageOptions={[10, 20, 50]}
+                            component="div"
+                            count={listUsers.length}
+                            rowsPerPage={rowsPerPage}
+                            page={page}
+                            onPageChange={handleChangePage}
+                            onRowsPerPageChange={handleChangeRowsPerPage}
+                        />
+                    </Paper>
+                </Grid>
+            }
             {
                 (modeModal === "update") ?
                     <Modal
@@ -238,7 +294,7 @@ export default function User() {
                                         <PersonIcon sx={{ margin: "0 1em -0.2em 0" }} /> <strong >Họ và tên</strong>
                                     </Grid>
                                     <Grid item xs={7} >
-                                        <Input sx={{ width: "90%" }} defaultValue={currentUser.username} />
+                                        <TextField key={1} defaultValue={currentUser.username} onChange={(e) => setEditUsername(e.target.value)} sx={{ width: "90%" }} />
                                     </Grid>
                                 </Grid>
                                 <Grid sx={{ display: "flex", textAlign: "left", padding: "1em 2em", alignItems: "center" }}>
@@ -246,7 +302,7 @@ export default function User() {
                                         <InfoIcon sx={{ margin: "0 1em -0.2em 0" }} /> <strong  >Id nhân viên</strong>
                                     </Grid>
                                     <Grid item xs={7} >
-                                        <Input sx={{ width: "90%" }} defaultValue={currentUser.id} />
+                                        <TextField key={3} disabled sx={{ width: "90%" }} defaultValue={currentUser.id} />
                                     </Grid>
                                 </Grid>
                                 <Grid sx={{ display: "flex", textAlign: "left", padding: "1em 2em", alignItems: "center" }}>
@@ -254,7 +310,15 @@ export default function User() {
                                         <MailIcon sx={{ margin: "0 1em -0.2em 0" }} /> <strong  >Email</strong>
                                     </Grid>
                                     <Grid item xs={7} >
-                                        <Input sx={{ width: "90%" }} defaultValue={currentUser.email} />
+                                        <TextField key={2} onChange={(e) => setEditUserEmail(e.target.value)} sx={{ width: "90%" }} defaultValue={currentUser.email} />
+                                    </Grid>
+                                </Grid>
+                                <Grid sx={{ display: "flex", textAlign: "left", padding: "1em 2em", alignItems: "center" }}>
+                                    <Grid item xs={5} >
+                                        <MailIcon sx={{ margin: "0 1em -0.2em 0" }} /> <strong  >Mật khẩu</strong>
+                                    </Grid>
+                                    <Grid item xs={7} >
+                                        <TextField key={4} onChange={(e) => setEditUserPassword(e.target.value)} sx={{ width: "90%" }} />
                                     </Grid>
                                 </Grid>
                                 <Grid sx={{ display: "flex", textAlign: "left", padding: "1em 2em", alignItems: "center" }}>
@@ -262,17 +326,19 @@ export default function User() {
                                         <LeaderboardIcon sx={{ margin: "0 1em -0.2em 0" }} /> <strong  >Vai trò</strong>
                                     </Grid>
                                     <RadioGroup
+                                        onChange={(e) => setEditUserRole([{ "name": e.target.value }])}
                                         aria-label="gender"
                                         defaultValue={currentUser.roles[0].name}
                                         name="radio-buttons-group"
                                         sx={{ marginLeft: "4em", display: "inline-block", alignItems: "center" }}
                                     >
-                                        <FormControlLabel value="STORAGE_KEEPER" control={<Radio />} label="Nhân viên nhập kho" />
-                                        <FormControlLabel value="ACCOUNTANT" control={<Radio />} label="Kế toán" />
+                                        <FormControlLabel value="ADMIN" control={<Radio />} label="Admin" />
+                                        <FormControlLabel value="Nhân viên kho" control={<Radio />} label="Nhân viên kho" />
+                                        <FormControlLabel value="Kế toán" control={<Radio />} label="Kế toán" />
                                     </RadioGroup>
                                 </Grid>
                                 <Grid sx={{ textAlign: "end", paddingTop: "2.4em" }}>
-                                    <Button variant="outlined" color="success" sx={{ fontSize: "0.8em", textTransform: 'capitalize', margin: "0.5em 0 2em 0" }}>Lưu thay đổi</Button>
+                                    <Button onClick={() => editAnUser()} variant="outlined" color="success" sx={{ fontSize: "0.8em", textTransform: 'capitalize', margin: "0.5em 0 2em 0" }}>Lưu thay đổi</Button>
                                     <Button onClick={() => setOpenModal(false)} variant="outlined" color="error" sx={{ fontSize: "0.8em", textTransform: 'capitalize', margin: "0.5em 2em 2em 2em" }}>Quay lại</Button>
                                 </Grid>
                             </Grid>
